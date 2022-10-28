c694dd0931e781a0bda87919c1862bb2834cbc85
availablenick <leoap711@gmail.com>
2022-10-27 22:33:59 -0300

    perf: improve bucket tool speed

--- client/src/res/tools/bucket.js
+++ client/src/res/tools/bucket.js
@@ -21,13 +21,13 @@ const bucket = {
       const green = Number(match[2]);
       const blue = Number(match[3]);
       const alpha = match[5] ? Number(match[5]) : 1;
-      const drawingColorComponents = [red, green, blue, alpha];
+      const drawingColorComponents = [red, green, blue, Math.round(alpha * 255)];
 
       const sourceX = event.clientX - canvas.offsetLeft;
       const sourceY = event.clientY - canvas.offsetTop;
       const sourcePixelData = context.getImageData(sourceX, sourceY, 1, 1).data;
       const sourceColorComponents = [
-        sourcePixelData[0], sourcePixelData[1], sourcePixelData[2], sourcePixelData[3] / 255,
+        sourcePixelData[0], sourcePixelData[1], sourcePixelData[2], sourcePixelData[3],
       ];
 
       fillMatchingArea(canvas, sourceX, sourceY, sourceColorComponents, drawingColorComponents);
@@ -39,10 +39,11 @@ const bucket = {
 
 function fillMatchingArea(canvas, sourceX, sourceY, sourceColorComponents, drawingColorComponents) {
   const context = canvas.getContext('2d');
-  if (!shouldFill(context, sourceX, sourceY, sourceColorComponents, drawingColorComponents)) {
+  const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+  if (!shouldFill(canvas, imageData.data, sourceX, sourceY, sourceColorComponents, drawingColorComponents)) {
     return;
   }
-
+  
   const queue = [[sourceX, sourceY]];
   while (queue.length > 0) {
     const pos = queue.shift();
@@ -50,30 +51,33 @@ function fillMatchingArea(canvas, sourceX, sourceY, sourceColorComponents, drawi
     const y = pos[1];
     let lx = x;
     while (lx >= 1
-      && shouldFill(context, lx - 1, y, sourceColorComponents, drawingColorComponents)) {
+      && shouldFill(canvas, imageData.data, lx - 1, y, sourceColorComponents, drawingColorComponents)) {
+      paintPixel(canvas, imageData.data, lx - 1, y, drawingColorComponents);
       --lx;
     }
 
     while (x < canvas.width
-      && shouldFill(context, x, y, sourceColorComponents, drawingColorComponents)) {
+      && shouldFill(canvas, imageData.data, x, y, sourceColorComponents, drawingColorComponents)) {
+      paintPixel(canvas, imageData.data, x, y, drawingColorComponents);
       ++x;
     }
 
-    context.fillRect(lx, y, x - lx, 1);
     if (y >= 1) {
-      scan(context, lx, x - 1, y - 1, queue, sourceColorComponents, drawingColorComponents);
+      scan(canvas, imageData.data, lx, x - 1, y - 1, queue, sourceColorComponents, drawingColorComponents);
     }
 
     if (y + 1 < canvas.width) {
-      scan(context, lx, x - 1, y + 1, queue, sourceColorComponents, drawingColorComponents);
+      scan(canvas, imageData.data, lx, x - 1, y + 1, queue, sourceColorComponents, drawingColorComponents);
     }
   }
+
+  context.putImageData(imageData, 0, 0);
 }
 
-function scan(context, lx, rx, y, queue, sourceColorComponents, drawingColorComponents) {
+function scan(canvas, pixelColors, lx, rx, y, queue, sourceColorComponents, drawingColorComponents) {
   let foundBoundary = true;
   for (let x = lx; x <= rx; ++x) {
-    if (!shouldFill(context, x, y, sourceColorComponents, drawingColorComponents)) {
+    if (!shouldFill(canvas, pixelColors, x, y, sourceColorComponents, drawingColorComponents)) {
       foundBoundary = true;
     } else if (foundBoundary) {
       queue.push([x, y]);
@@ -82,11 +86,26 @@ function scan(context, lx, rx, y, queue, sourceColorComponents, drawingColorComp
   }
 }
 
-function shouldFill(context, x, y, sourceColorComponents, drawingColorComponents) {
-  const pixelData = context.getImageData(x, y, 1, 1).data;
-  const components = [pixelData[0], pixelData[1], pixelData[2], pixelData[3] / 255];
+function paintPixel(canvas, pixelColors, x, y, colorComponents) {
+  const index = calculateIndex(canvas.width, x, y);
+  pixelColors[index] = colorComponents[0];
+  pixelColors[index + 1] = colorComponents[1];
+  pixelColors[index + 2] = colorComponents[2];
+  pixelColors[index + 3] = colorComponents[3];
+}
+
+function shouldFill(canvas, pixelColors, x, y, sourceColorComponents, drawingColorComponents) {
+  const index = calculateIndex(canvas.width, x, y);
+  const components = [
+    pixelColors[index], pixelColors[index + 1], pixelColors[index + 2], pixelColors[index + 3],
+  ];
+
   return colorsMatch(components, sourceColorComponents)
         && !colorsMatch(components, drawingColorComponents);
+}
+
+function calculateIndex(numberOfColumns, x, y) {
+  return (numberOfColumns * y + x) * 4;
 }
 
 function colorsMatch(color1Components, color2Components) {
