e20ffd5a64ae0bab534273f9628205b3519a586f
availablenick <leoap711@gmail.com>
2022-10-31 20:43:48 -0300

    fix: adjust user pointer position to match the marker's center

--- client/src/Pointer.js
+++ client/src/Pointer.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Pointer.scss';
 
 function Pointer({ userId, x, y, color = '#000' }) {
+  const markerRef = useRef(null);
+  const [style, setStyle] = useState({});
+
+  useEffect(() => {
+    if (markerRef.current) {
+      const marker = markerRef.current;
+      setStyle({
+        left: x - marker.offsetLeft - marker.offsetWidth / 2,
+        top: y - marker.offsetTop - marker.offsetHeight / 2,
+      });
+    }
+  }, [x, y]);
+
   return (
-    <div
-      className="user-pointer"
-      style={{ left: x, top: y }}
-    >
+    <div className="user-pointer" style={style}>
       <span className="label" style={{ color }}>
         User {userId}
       </span>
-      <span className="marker" style={{ background: color }} />
+      <span className="marker" ref={markerRef} style={{ background: color }} />
     </div>
   );
 }
