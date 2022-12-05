21f4872d3f3ec22018ebbd7cd738910e517eace0
availablenick <leoap711@gmail.com>
2022-12-05 14:01:12 -0300

    test: add tests for client communication

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const { createServer } = require('http');
 const { Server } = require('socket.io');
+const setUpListeners = require('./src/communication.service');
 
 const allowedURLS = process.env.ALLOWED_URLS.split(',');
 
@@ -12,34 +13,7 @@ const io = new Server(httpServer, {
   }
 });
 
-const clientData = {};
-
-io.of(/\/(\w+)/).on("connection", (socket) => {
-  if (!clientData[socket.nsp.name]) {
-    clientData[socket.nsp.name] = {};
-  }
-
-  socket.on('new-client', (data) => {
-    clientData[socket.nsp.name][socket.id] = { x: 0, y: 0, color: data };
-    socket.nsp.emit('client-connection-change', clientData[socket.nsp.name]);
-
-    socket.on('canvas-change', (data) => {
-      socket.broadcast.emit('canvas-change', data);
-    });
-  
-    socket.on('client-position-change', (data) => {
-      clientData[socket.nsp.name][socket.id].x = data.x;
-      clientData[socket.nsp.name][socket.id].y = data.y;
-      const position = { id: socket.id, x: data.x, y: data.y };
-      socket.broadcast.emit('client-position-change', position);
-    });
-  
-    socket.on('disconnect', () => {
-      delete clientData[socket.nsp.name][socket.id];
-      socket.nsp.emit('client-connection-change', clientData[socket.nsp.name]);
-    });
-  });
-});
+setUpListeners(io);
 
 httpServer.listen(5000, () => {
   console.log(`Listening on port 5000`);
